5f8853af2869bfbc6f620359365f6134bcdf08a4
mayman <m.ayman@tamkeentech.sa>
2022-05-12 21:46:14 +0300

    anonymous vs lambda

---
+++ src/main/java/com/anchtun/java8/anonymousvslambda/ArithmeticMain.java
@@ -0,0 +1,26 @@
+package com.anchtun.java8.anonymousvslambda;
+
+public class ArithmeticMain {
+
+	public static void main(String[] args) {
+		int c = 5;
+		int d = 12;
+		
+		// anonymous class: works with interfaces that have many abstract methods
+		// its anonymous inner class without name
+		ArithmeticOperation arthOpt1 = new ArithmeticOperation() {
+			@Override
+			public int printResult(int a, int b) {
+				return a + b;
+			}
+		};
+		
+		System.out.println("Result of addition using anonymous class is: " + arthOpt1.printResult(c, d));
+		
+		// lambda expression: works with functional interfaces only
+		// its anonymous method without name
+		ArithmeticOperation arthOpt2 = (a, b) -> a + b;
+		System.out.println("Result of addition using lambda expression is: " + arthOpt2.printResult(c, d));
+
+	}
+}
--- src/main/java/com/anchtun/java8/anonymousvslambda/ArithmeticOperation.java
+++ src/main/java/com/anchtun/java8/anonymousvslambda/ArithmeticOperation.java
@@ -1,8 +1,8 @@
-package com.anchtun.java8.lambdaexpression;
+package com.anchtun.java8.anonymousvslambda;
 
 // this annotation is optional; just to help you to know that this interface is a Functional Interface
-// but when we add it the compiler avoid you to add two abstract methods
-//@FunctionalInterface
+//but when we add it the compiler avoid you to add two abstract methods
+@FunctionalInterface
 public interface ArithmeticOperation {
 
 	int printResult(int a, int b);
