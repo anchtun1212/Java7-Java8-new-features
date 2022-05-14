9a9529c2a5def6b5c35cb46e2cc3e43e6ad7b914
mayman <m.ayman@tamkeentech.sa>
2022-05-14 12:12:49 +0300

    pipeline

--- src/main/java/com/anchtun/java8/streams/FirstStream.java
+++ src/main/java/com/anchtun/java8/streams/FirstStream.java
@@ -20,21 +20,46 @@ public class FirstStream {
 		Stream<Integer> favoriteNumbersStream = favoriteNumbers.stream();
 		System.out.println("Show numbers using stream():");
 		favoriteNumbersStream.forEach(System.out::println);
-		
+
 		// Convert favoriteNumbers list to parallelStream
 		Stream<Integer> favoriteNumbersParralelStream = favoriteNumbers.parallelStream();
 		System.out.println("Show numbers using parallelStream():");
 		favoriteNumbersParralelStream.forEach(System.out::println);
-		
+
 		// use Stream.of(T)
 		Stream<Integer> favoriteNumbersOfStream = Stream.of(5, 12, 13, 22, 8);
 		System.out.println("Show numbers using Stream.of(T):");
 		favoriteNumbersOfStream.forEach(System.out::println);
-		
+
 		// create empty Stream
 		Stream<Integer> emptyStream = Stream.empty();
 		System.out.println("empty Stream:");
 		emptyStream.forEach(System.out::println);
+
+		// create Stream from Array
+		Integer[] fullArray = { 5, 12, 13, 22, 8 };
+		Stream<Integer> streamFromArray = Arrays.stream(fullArray);
+		System.out.println("Show numbers using Arrays.stream(fullArray):");
+		streamFromArray.forEach(System.out::println);
+
+		// create Stream from Part of Array
+		Integer[] partOfArray = { 5, 12, 13, 22, 8 };
+		Stream<Integer> streamFromPartOfArray = Arrays.stream(partOfArray, 2, 5);
+		System.out.println("Show numbers using Arrays.stream(partOfArray, from, to):");
+		streamFromPartOfArray.forEach(System.out::println);
+
+		// generate an infinite stream of element
+		//Supplier<Integer> supplier = () -> 512;
+		//Stream<Integer> streamInfiniteGenerate = Stream.generate(supplier);
+		//System.out.println("Show numbers using Stream.generate (generate an infinite stream of element):");
+		//streamInfiniteGenerate.forEach(System.out::println);
+		
+		// iterate infinite elements
+		//UnaryOperator<Integer> multiplyBy5 = input -> input*5;
+		//multiplyBy5.apply(5);
+		//Stream<Integer> streamIterate = Stream.iterate(2, multiplyBy5);
+		//System.out.println("Show numbers using Stream.iterate:");
+		//streamIterate.forEach(System.out::println);
 	}
 
 }
